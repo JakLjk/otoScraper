6a5e9d28d3925b49e239fdca85a90c5c1fb05cad
pomeran <lejk99@icloud.com>
2024-11-16 08:43:46 +0100

    Added requirements.txt

--- driver.py
+++ driver.py
@@ -2,6 +2,7 @@ from typing import Literal
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options as firefoxOptions
 from selenium.webdriver.remote.webdriver import WebDriver
+from webdriver_manager.firefox import GeckoDriverManager
 
 
 
@@ -13,6 +14,6 @@ def initialise_selenium(browser_type:Literal["firefox"],
         "firefox":firefoxOptions}
     
     options = accepted_options[browser_type]()
-    options.headless=headless
+    options.add_argument('--headless')
     return accepted_drivers[browser_type](options=options)
 
